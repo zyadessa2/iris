1679e0e708c5d52a47113f226d647bb3d8522762
ziad essa <helmyzez@gmail.com>
2025-07-26 00:21:50 +0300

    fix images err

--- src/app/_components/Events.tsx
+++ src/app/_components/Events.tsx
@@ -18,28 +18,28 @@ const data = [
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://iriseventsmgt.com/nw/iris/images/Past%20Events/cardio-egypt.jpg"
+              src="/images/cardio-egypt.jpg"
               alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-5.fna.fbcdn.net/v/t39.30808-6/494415220_1225124875637011_8926575263196910814_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_ohc=SZtKXZZRc-sQ7kNvwESZ5tA&_nc_oc=AdkPC90oXVJLBnhT4md8SOClTPkS9hIvE-DizynI2mCiPzuadfaeIn6tfn9CBRK7RXU&_nc_zt=23&_nc_ht=scontent.fcai20-5.fna&_nc_gid=q9vcRldOu-n_zn1cckV5xg&oh=00_AfFvocvTxPjVEGDpTjcFcFj103VTDBiQMbB-NWnu2lXoVg&oe=68193136"
+              src="/images/gulf.jpg"
               alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-4.fna.fbcdn.net/v/t39.30808-6/494289437_1225123748970457_2128120968907178194_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=NnZ1hqxNHeAQ7kNvwGsF2uO&_nc_oc=Admpujdy9GCSatChN9UBrnWuFpSIh29_REt6fY-hl1LHQ9oqpCEuZLusV5S2Li7MTPw&_nc_zt=23&_nc_ht=scontent.fcai20-4.fna&_nc_gid=q9vcRldOu-n_zn1cckV5xg&oh=00_AfGAdFm7GpYORsy6CYGA8RqD9ZazYZQa4krrJlAnB6UFZA&oe=6819571F"
+              src="/images/tourism.jpeg"
               alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-1.fna.fbcdn.net/v/t39.30808-6/495014520_1020279013582763_6962999629587775066_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_ohc=HtryCG0YhbEQ7kNvwF72X6O&_nc_oc=Adn-3iQzpiytMTRjdGBMDiGex2190ZB8zXZGnOrcBqFxgo5T42h4LvKHAR8rCJzybF8&_nc_zt=23&_nc_ht=scontent.fcai20-1.fna&_nc_gid=W9AHQbi43LJ_RAa7d5WJiw&oh=00_AfFtTxZ00FsrNfc5csvos8U5kEAHwXW1D32joGPB1gSwtg&oe=681946E3"
+              src="/images/coventry.jpg"
               alt="startup template"
               width={500}
               height={500}
@@ -66,29 +66,29 @@ const data = [
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://iriseventsmgt.com/nw/iris/images/Past%20Events/gulf.jpg"
-              alt="hero template"
+              src="/images/cardio-egypt.jpg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-2.fna.fbcdn.net/v/t39.30808-6/494680977_1020278983582766_3118287496756553501_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_ohc=8W3JXuCQFi0Q7kNvwHm9Mjo&_nc_oc=Adkzi5cuP_xyWfFqm2byzOu1dD9Mf5oPhpmgthWH5ssVrUgnjnWd2hMRHys76U5F_hY&_nc_zt=23&_nc_ht=scontent.fcai20-2.fna&_nc_gid=W9AHQbi43LJ_RAa7d5WJiw&oh=00_AfGI-3XCMuuG6QWtNt-k2OVYKUweYHf6VIEKlPnj4ar1jQ&oe=681935DE"
-              alt="feature template"
+              src="/images/gulf.jpg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-4.fna.fbcdn.net/v/t39.30808-6/495142854_1020273063583358_336358845805543473_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=107&ccb=1-7&_nc_sid=f727a1&_nc_ohc=8f51RRHV7IYQ7kNvwHOuYzy&_nc_oc=AdnLbqnEo3iEU8s4Jfxy4iGlbhUicU3Dl-6OzCVxt0MeZFSpOMdefQwYydKU8JtjOow&_nc_zt=23&_nc_ht=scontent.fcai20-4.fna&_nc_gid=W9AHQbi43LJ_RAa7d5WJiw&oh=00_AfHx5FldX25o_h1xa3wUW2KXlIRrrROPjAqRl5VmE3QYcg&oe=68194C63"
-              alt="bento template"
+              src="/images/tourism.jpeg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-5.fna.fbcdn.net/v/t39.30808-6/494074904_1020258976918100_8764798111707171802_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=106&ccb=1-7&_nc_sid=f727a1&_nc_ohc=9jndFSJNEn8Q7kNvwE_SerQ&_nc_oc=AdllJJZnL52fet7oIqpoRXKHywaGEZ0g7wPFszXGss2SDCO_emOllM7CFcDHdK0bV4o&_nc_zt=23&_nc_ht=scontent.fcai20-5.fna&_nc_gid=gUWND_zQFuZENEAcaxiNRA&oh=00_AfEu4NHJuaWBQ74gP8WN6Jjwg94_5pkbcxr6UxAR35mBSg&oe=6819321B"
-              alt="cards template"
+              src="/images/coventry.jpg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
@@ -109,29 +109,29 @@ const data = [
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://iriseventsmgt.com/nw/iris/images/Past%20Events/coventry.jpg"
-              alt="hero template"
+              src="/images/cardio-egypt.jpg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-1.fna.fbcdn.net/v/t39.30808-6/494104684_1020226480254683_8074629173785074361_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_ohc=w-MPK9qgycsQ7kNvwGjaU7u&_nc_oc=Adkv0XFfoygEpn_6imErILVyuv_861RmZkF3rPEvDam3HTWYfy-kXpEWVJIB5XAHf6M&_nc_zt=23&_nc_ht=scontent.fcai20-1.fna&_nc_gid=Q7mB5Nz0vVV0S-wxAgUiyw&oh=00_AfFp8XqJGOQOh1ezdd7QyF2w5Idbsj0--WXJNJQXQru3dQ&oe=6819355D"
-              alt="feature template"
+              src="/images/gulf.jpg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-5.fna.fbcdn.net/v/t39.30808-6/494073618_1020235450253786_8155348158456455978_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_ohc=lE14CWoLsPQQ7kNvwE_h9MG&_nc_oc=Adm6fGBKPiIXcud1EFvkwSeJW3UMelQyCdYCsM1wv1LTdsd3R1cI3s7i-6gyJTYACP8&_nc_zt=23&_nc_ht=scontent.fcai20-5.fna&_nc_gid=Q7mB5Nz0vVV0S-wxAgUiyw&oh=00_AfHv6spbBp4SNmUrwkv7y1ll2tzVLy7CEbhRYxkmYgpkYw&oe=68194E97"
-              alt="bento template"
+              src="/images/tourism.jpeg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-2.fna.fbcdn.net/v/t39.30808-6/494685942_1020241336919864_2734500190488574405_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_ohc=OBykP4VugzUQ7kNvwFP6iEY&_nc_oc=AdlwHPPentkJWBbWxl4tYC_AX9F7ZRKU5z8avyL3ZRMD2Jkl3TlZTrFmvAqxJajtcD4&_nc_zt=23&_nc_ht=scontent.fcai20-2.fna&_nc_gid=gUWND_zQFuZENEAcaxiNRA&oh=00_AfEwuHqXL7xUs77HOf2PnFrFOANCKScqDUasUcnpHDdirg&oe=68192FC6"
-              alt="cards template"
+              src="/images/coventry.jpg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
@@ -152,29 +152,29 @@ const data = [
           </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
-              src="https://iriseventsmgt.com/nw/iris/images/Past%20Events/tourism.jpeg"
-              alt="hero template"
+              src="/images/cardio-egypt.jpg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-3.fna.fbcdn.net/v/t39.30808-6/494014750_1214953174008434_8098493252665005179_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=li6jrqm7ahoQ7kNvwGkM90o&_nc_oc=AdmD1SFuZzMH2poZ-V1sDG3Kn-QrjkrfcxemCNIUi_yAX7kEpoKvM1qljgIKpCH2vag&_nc_zt=23&_nc_ht=scontent.fcai20-3.fna&_nc_gid=etaa8uVdmUTrrE1wU8RwTQ&oh=00_AfHWX-Zm8VsJsIG0zEZOh04h4SdfUhjJW05yxQW4MDJgaQ&oe=68195855"
-              alt="feature template"
+              src="/images/gulf.jpg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-6.fna.fbcdn.net/v/t39.30808-6/494893456_1020225376921460_1002531635003455487_n.jpg?stp=dst-jpg_p180x540_tt6&_nc_cat=109&ccb=1-7&_nc_sid=f727a1&_nc_ohc=Stf5PwTYY3UQ7kNvwGqYGcw&_nc_oc=AdkOnBh_cwVqIaSHn5hBoilPenaFxgDVtQ5IfsPD7M7cUvsHP_B_TjS3oPOlbisc5ns&_nc_zt=23&_nc_ht=scontent.fcai20-6.fna&_nc_gid=Q7mB5Nz0vVV0S-wxAgUiyw&oh=00_AfGdpvDQ0PPzYL4RxVnQKgNr_tCeM2GfXa9R2hHp4D6OBw&oe=681961A2"
-              alt="bento template"
+              src="/images/tourism.jpeg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <Image
-              src="https://scontent.fcai20-3.fna.fbcdn.net/v/t39.30808-6/494755528_1020226596921338_2858704632294756610_n.jpg?stp=dst-jpg_p526x395_tt6&_nc_cat=111&ccb=1-7&_nc_sid=f727a1&_nc_ohc=H9NvboDUhAcQ7kNvwH_9Gd2&_nc_oc=AdnAKRXWw37Z14q1dCIrzBCJ_7Re-1LvjEaP-VtB_ouXDdx7NGWTjjlu__0HS5beMVg&_nc_zt=23&_nc_ht=scontent.fcai20-3.fna&_nc_gid=Q7mB5Nz0vVV0S-wxAgUiyw&oh=00_AfGLWUSwNtEUyk1RF0uq9oON1CaXc0fhyLDlf-T3bjqRyg&oe=68195A40"
-              alt="cards template"
+              src="/images/coventry.jpg"
+              alt="startup template"
               width={500}
               height={500}
               className="h-50 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
